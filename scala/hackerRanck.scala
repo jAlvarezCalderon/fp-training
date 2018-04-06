db6fa1ee727b54dadaebedcb5c08ca820ee6df8d
JImmy Alejandro Alvarez <alejandro24alvarez@gmail.com>
2018-04-06 13:20:10 -0500

    Add solution for filter-positions-in-a-list

--- scala/hackerRanck.scala
+++ scala/hackerRanck.scala
@@ -19,5 +19,16 @@ def f(arr: List[Int])(predicate: Int => Boolean): List[Int] = {
   loop(arr, List())
 }
 
+// https://www.hackerrank.com/challenges/fp-filter-positions-in-a-list/problem
+
+def f(arr:List[Int]):List[Int] = filterBaseOnIndex(arr)(x => x % 2 == 0)
+
+def filterBaseOnIndex(arr:List[Int])(cond: Int => Boolean): List[Int] = {
+    def loop(index:Int, cl:List[Int], nl:List[Int]): List[Int] = cl match {
+        case Nil => nl
+        case h :: t => if(cond(index)) loop(index +1, t, nl ::: List(h) ) else loop(index +1, t, nl)
+    }
+    loop(1,arr,List())
+}
 
 
